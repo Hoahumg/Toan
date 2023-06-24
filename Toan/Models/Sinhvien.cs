7fcd05d7fddd56ba8a41cd23f65f49e8ef75b4ef
Hoahumg <lethihoa18032001@gamil.com>
2023-06-24 11:56:02 +0700

    Cau 1

---
+++ Toan/Models/Sinhvien.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+namespace Toan.Models;
+public class Sinhvien
+{
+    [Key]
+    [Display(Name = "Mã sinh viên")]
+    public string Masinhvien {get; set;}
+    [Display(Name = "Họ tên")]
+    public string Hoten {get; set;}
+    [Display(Name = "Mã lớp")]
+    public string Malop {get; set;}
+
+}
